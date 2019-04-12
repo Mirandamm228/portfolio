94b011d92ad6725602b37b85c3efb7c0ad017b3e
Miranda <miranda@greenfields.io>
2019-04-12 16:26:00 -0700

    restyled landing page component using mobile first

--- client/src/components/landing-page/LandingPage.js
+++ client/src/components/landing-page/LandingPage.js
@@ -5,10 +5,10 @@ class LandingPage extends React.Component{
     render(){
         return(
             <div className="landingPage-image">
-                <div className="vertical-align text-right">
-                    <h1 className="landingPage-header">You need a <span className="emphasis-color">developer</span> who is..</h1>
+                <div className="vertical-align text-left">
+                    <h1 className="landingPage-header">You need a <span className="emphasis-color">developer</span> who is -</h1>
                     <br />
-                    <h3><span className="emphasis-color txt-sm">Determined, a problem solver, <br /> always improving, and honest... <br />Look no further!</span></h3>
+                    <h3><span className="emphasis-color txt-sm">determined, a problem solver, always improving, and honest... <br />Look no further!</span></h3>
                     <a href="/aboutme" aria-label=""><button className="btn btn-primary btn-lg mt-3" aria-label="Learn more">Learn more</button></a>
                 </div>
             </div>
