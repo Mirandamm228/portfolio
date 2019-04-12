b65433a416826031da779d8d5ba315fa41cce173
Miranda <miranda@greenfields.io>
2019-04-12 11:56:50 -0700

    attempt #2 to resolve invalid host header issue

--- server.js
+++ server.js
@@ -9,3 +9,12 @@ app.listen(port, () => console.log(`Listening on port ${port}`));
 app.get('/express_backend', (req, res) => {
   res.send({ express: 'YOUR EXPRESS BACKEND IS CONNECTED TO REACT' });
 });
+
+if (process.env.NODE_ENV === "production") {
+  // Set static folder
+  app.use(express.static("client/build"));
+
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+  });
+}
