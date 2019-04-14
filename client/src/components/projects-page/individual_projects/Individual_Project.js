ab827fd0bb34b8dc30307df9e32a09957c322e55
Miranda <miranda@greenfields.io>
2019-04-14 08:20:09 -0700

    edits of the homepage property of the github api

--- client/src/components/projects-page/individual_projects/Individual_Project.js
+++ client/src/components/projects-page/individual_projects/Individual_Project.js
@@ -15,7 +15,7 @@ const IndividualProject = (props) => (
                     <h3 className="card-title">Name: {props.repo.name}</h3>
                     <p className="card-text">Description: {props.repo.description}</p>
                     <a rel="noopener noreferrer" href={props.repo.html_url} target="_blank"><i className="fab fa-github-square fa-3x m-2"></i></a>
-                    {props.repo.homepage !== null ? <a href={props.repo.homepage} target="_blank" rel="noopener noreferrer">Visit Site</a> : null}
+                    {props.repo.homepage !== null && props.repo.homepage !== "" ? <a href={props.repo.homepage} target="_blank" rel="noopener noreferrer">Visit Site</a> : null}
                     </div>
                 </div>
             </Router>
